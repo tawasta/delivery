99919b999b111883a1fe61fb81dcb4282560cd83
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-01-03 14:12:36 +0200

    [FIX] Override the correct method

--- delivery_gls_finland_delivery_email/models/delivery_carrier.py
+++ delivery_gls_finland_delivery_email/models/delivery_carrier.py
@@ -5,7 +5,7 @@ class DeliveryCarrier(models.Model):
 
     _inherit = "delivery.carrier"
 
-    def _gls_finland_map_record(self, picking):
+    def _get_email_pre_advice_email(self, picking):
         email = (
             picking.partner_id.email_delivery
             or (picking.sale_id and picking.sale_id.partner_id.email_delivery)
