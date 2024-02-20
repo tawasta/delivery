63310430189b8e0121e6a15daa5ffe45ae9d204e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-02-20 14:14:17 +0200

    [MIG] Init 17.0

--- delivery_gls_finland/models/product_packaging.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ProductPackaging(models.Model):
-    _inherit = "product.packaging"
-
-    package_carrier_type = fields.Selection(
-        selection_add=[("gls_finland", "GLS Finland")]
-    )
--- delivery_gls_finland_delivery_email/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-
-    _inherit = "res.partner"
-
-    email_delivery = fields.Char(string="Delivery Email", copy=False, store=True)
