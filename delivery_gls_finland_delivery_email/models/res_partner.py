ed5c8df128701eb44083ab3e20a4d1e88b4d3430
difbit <timo.kekalainen@tawasta.fi>
2025-03-29 10:30:12 +0200

    [IMP] Removed print

---
+++ delivery_gls_finland_delivery_email/models/res_partner.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    email_delivery = fields.Char(string="Delivery Email", copy=False, store=True)
