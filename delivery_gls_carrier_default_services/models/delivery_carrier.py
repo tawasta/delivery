c5fd221a027736d3af661321d9ed5b8df01993a4
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-10 14:25:38 +0300

    T72789-ennakkoilmoitukset

---
+++ delivery_gls_carrier_default_services/models/delivery_carrier.py
@@ -0,0 +1,20 @@
+import logging
+
+from odoo import _, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class DeliveryCarrier(models.Model):
+    _inherit = "delivery.carrier"
+
+    picking_order_autoadd_services_gls_finland_service_ids = fields.Many2many(
+        "gls.finland.service",
+        relation="delivery_gls_carrier_default_services_service_ids",
+        string="Add services to picking order when created from sale order"
+    )
+    picking_order_autoadd_if_customer_email_services_gls_finland_service_ids = fields.Many2many(
+        "gls.finland.service",
+        relation="delivery_gls_carrier_if_email_default_services_service_ids",
+        string="Add services to picking order when created from sale order if customer has delivery email set"
+    )
