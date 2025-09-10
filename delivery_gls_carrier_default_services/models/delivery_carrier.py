dd1af43fb86b9733ae56ef37507648e300563aee
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-10 14:34:44 +0300

    Add translations

--- delivery_gls_carrier_default_services/models/delivery_carrier.py
+++ delivery_gls_carrier_default_services/models/delivery_carrier.py
@@ -16,5 +16,5 @@ class DeliveryCarrier(models.Model):
     picking_order_autoadd_if_customer_email_services_gls_finland_service_ids = fields.Many2many(
         "gls.finland.service",
         relation="delivery_gls_carrier_if_email_default_services_service_ids",
-        string="Add services to picking order when created from sale order if customer has delivery email set"
+        string="Add services to picking order when created from sale order if customer has email set"
     )
