5ddd15f759af7c18ecf4fb821ae4c4351dc288ac
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-10 14:55:52 +0300

    Precommit fixes

--- delivery_gls_carrier_default_services/models/stock_picking.py
+++ delivery_gls_carrier_default_services/models/stock_picking.py
@@ -16,7 +16,7 @@ class StockPicking(models.Model):
                 # If customer has email and carrier has services
                 if sale_order["partner_id"] \
                         and sale_order["partner_id"]["email"] \
-                        and sale_order.carrier_id.picking_order_autoadd_if_customer_email_services_gls_finland_service_ids.
+                        and sale_order.carrier_id.picking_order_autoadd_if_customer_email_services_gls_finland_service_ids:
                     picking.gls_finland_service_ids = sale_order.carrier_id.picking_order_autoadd_if_customer_email_services_gls_finland_service_ids
                 # If carrier has services
                 elif sale_order.carrier_id.picking_order_autoadd_services_gls_finland_service_ids:
