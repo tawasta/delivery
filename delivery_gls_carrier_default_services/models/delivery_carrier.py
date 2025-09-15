8d09e54f66b2d092f76f8191162f2fd8a6c87efb
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-15 13:19:50 +0300

    Precommit fixes

--- delivery_gls_carrier_default_services/models/delivery_carrier.py
+++ delivery_gls_carrier_default_services/models/delivery_carrier.py
@@ -19,8 +19,10 @@ class DeliveryCarrier(models.Model):
         string="Add services to picking order when created from sale order if customer has email set",
     )
 
-    picking_order_autoadd_when_chosen_services_gls_finland_service_ids = fields.Many2many(
-        "gls.finland.service",
-        relation="delivery_gls_carrier_when_chosen_default_services_service_ids",
-        string="Add services to picking order when this carrier is chosen",
+    picking_order_autoadd_when_chosen_services_gls_finland_service_ids = (
+        fields.Many2many(
+            "gls.finland.service",
+            relation="delivery_gls_carrier_when_chosen_default_services_service_ids",
+            string="Add services to picking order when this carrier is chosen",
+        )
     )
