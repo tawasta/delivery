087c32c25dae1e8856efed435db2eca3b7d5d1a9
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-15 07:22:51 +0300

    T72789-ennakkoilmoitukset (#14)
    * T72789-ennakkoilmoitukset

---
+++ delivery_gls_carrier_default_services/models/stock_picking.py
@@ -0,0 +1,27 @@
+import logging
+
+from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.model_create_multi
+    def create(self, values_list):
+        pickings = super().create(values_list)
+        for picking in pickings:
+            sale_order = self.env["sale.order"].search([("name", "=", picking.origin)])
+            if sale_order.carrier_id:
+                # If customer has email and carrier has services
+                if (
+                    sale_order["partner_id"]
+                    and sale_order["partner_id"]["email"]
+                    and sale_order.carrier_id.picking_order_autoadd_if_customer_email_services_gls_finland_service_ids
+                ):
+                    picking.gls_finland_service_ids = sale_order.carrier_id.picking_order_autoadd_if_customer_email_services_gls_finland_service_ids
+                # If carrier has services
+                elif sale_order.carrier_id.picking_order_autoadd_services_gls_finland_service_ids:
+                    picking.gls_finland_service_ids = sale_order.carrier_id.picking_order_autoadd_services_gls_finland_service_ids
+        return picking
